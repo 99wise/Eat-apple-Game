e45b549f2e853e6d29ad0ff4aef246aeeaaf9670
joohee4382 <wise4382@gmail.com>
2021-01-22 00:02:35 +0900

    좀비 움직임 생성

--- ZombieMobGame.py
+++ ZombieMobGame.py
@@ -1,4 +1,4 @@
-﻿ssimport itertools, sys, time, random, math, pygame
+﻿import itertools, sys, time, random, math, pygame
 from pygame.locals import *
 from MyLibrary import *
 import random
@@ -35,8 +35,10 @@ timer = pygame.time.Clock()
 
 player_group = pygame.sprite.Group()
 food_group = pygame.sprite.Group()
-larva_group = pygame.sprite.Group()
-potion_group = pygame.sprite.Group()
+zombie_group = pygame.sprite.Group()
+
+# larva_group = pygame.sprite.Group()
+# potion_group = pygame.sprite.Group()
 
 #게이머 마법사 그룹 초기화
 player = MySprite()
@@ -45,18 +47,11 @@ player.position = 80, 80
 player.direction = 4
 player_group.add(player)
 
-
-for n in range(1,3):
-    larva = MySprite()
-    larva.load("larva.png", 40, 40, 1)
-    larva.position = random.randint(0,780),random.randint(0,580)
-    larva_group.add(larva)
-#포션 이미지 추가
-for n in range(1,50):
-    potion = MySprite()
-    potion.load("potion.png",65,65,15)
-    potion.position = random.randint(0,780),random.randint(0,580)
-    potion_group.add(potion)
+zombie = MySprite()
+zombie.load("zombie walk.png", 96, 96, 8)
+zombie.position = random.randint(0,780),random.randint(0,580)
+player.direction = 4
+zombie_group.add(zombie)
 
 #초기화 음식요정그룹
 
@@ -69,6 +64,7 @@ for n in range(1,50):
 game_over = False
 player_moving = False
 player_health = 0
+zombie_moving = False
 
 
 while True:
@@ -81,28 +77,51 @@ while True:
             sys.exit()
     keys = pygame.key.get_pressed()
     if keys[K_ESCAPE]: sys.exit()
-    elif keys[K_UP] or keys[K_w]:
+    elif keys[K_UP]:
         player.direction = 0
         player_moving = True
-    elif keys[K_RIGHT] or keys[K_d]:
+    elif keys[K_RIGHT]:
         player.direction = 2
         player_moving = True
-    elif keys[K_DOWN] or keys[K_s]:
+    elif keys[K_DOWN]:
         player.direction = 4
         player_moving = True
-    elif keys[K_LEFT] or keys[K_a]:
+    elif keys[K_LEFT]:
         player.direction = 6
         player_moving = True
     else:
         player_moving = False
+
+    if keys[K_ESCAPE]: sys.exit()
+    elif keys[K_w]:
+        zombie.direction = 0
+        zombie_moving = True
+    elif keys[K_d]:
+        zombie.direction = 2
+        zombie_moving = True
+    elif keys[K_s]:
+        zombie.direction = 4
+        zombie_moving = True
+    elif keys[K_a]:
+        zombie.direction = 6
+        zombie_moving = True
+    else:
+        zombie_moving = False
 
 
     if not game_over:
         #캐릭터의 다양한 방향에 따라, 각기 다른 애니메이션 프레임워크를 사용한다.
         player.first_frame = player.direction * player.columns
         player.last_frame = player.first_frame + player.columns-1
+
+        zombie.first_frame = zombie.direction * zombie.columns
+        zombie.last_frame = zombie.first_frame + zombie.columns-1
+
         if player.frame < player.first_frame:
             player.frame = player.first_frame
+        
+        if zombie.frame < zombie.first_frame:
+            zombie.frame = zombie.first_frame
 
         if not player_moving:
             #버튼(인물이 이동하는 것을 멈출 때)을 멈추고 애니메이션 프레임 업데이트를 중단합니다
@@ -112,8 +131,18 @@ while True:
             player.velocity.x *= 2
             player.velocity.y *= 2
 
+            
+        if not zombie_moving:
+            #버튼(인물이 이동하는 것을 멈출 때)을 멈추고 애니메이션 프레임 업데이트를 중단합니다
+            zombie.frame = zombie.first_frame = zombie.last_frame
+        else: 
+            zombie.velocity = calc_velocity(zombie.direction, 2)
+            zombie.velocity.x *= 2
+            zombie.velocity.y *= 2
+
         #게이머 마법사 그룹 업데이트
         player_group.update(ticks, 50)
+        zombie_group.update(ticks, 50)
 
         #모바일 플레이어
         if player_moving:
@@ -123,6 +152,14 @@ while True:
             elif player.X > 700: player.X = 700
             if player.Y < 0: player.Y = 0
             elif player.Y > 500: player.Y = 500
+
+        if zombie_moving:
+            zombie.X += zombie.velocity.x
+            zombie.Y += zombie.velocity.y
+            if zombie.X < 0: zombie.X = 0
+            elif zombie.X > 700: zombie.X = 700
+            if zombie.Y < 0: zombie.Y = 0
+            elif zombie.Y > 500: zombie.Y = 500
 
         #플레이어가 음식과 충돌하는지, 열매를 먹는지 검사합니다.
         attacker = None
@@ -135,19 +172,23 @@ while True:
         #푸드 요정 팀 업데이트
         food_group.update(ticks, 50)
 
+
         if len(food_group) == 0:
             game_over = True
+
     #텔레비전 화면을 깨끗이 하다
     screen.fill((50,50,100))
 
     #요정을 그리다
     food_group.draw(screen)
     player_group.draw(screen)
-    larva_group.draw(screen)
+    zombie_group.draw(screen)
 
     #플레이어 혈행 그리기
     pygame.draw.rect(screen, (50,150,50,180), Rect(300,570,player_health*2,25))
     pygame.draw.rect(screen, (100,200,100,180), Rect(300,570,200,25), 2)
+
+    
 
     if game_over:
         print_text(font, 300, 100, "G A M E   O V E R")
